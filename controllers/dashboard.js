5d0e936b070ad91caddf7db0caca8cf0674cb849
Austin_Curran <a.curran1192@gmail.com>
2022-09-24 20:26:36 -0600

    added section for statistics, total pay working

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,11 +1,23 @@
 const Task = require('../models/Tasks')
+const Entries = require('../models/Entries')
 
 module.exports = {
     getDashboard: async (req,res)=>{
         console.log(req.user)
         try{
+            // Need to format time to a number and calculate pay per hour
             const taskItems = await Task.find({userId:req.user.id})
-            res.render('dashboard.ejs', {tasks: taskItems, user: req.user})
+            const entryItems = await Entries.find({userId:req.user.id})
+            const totalTimes = entryItems.reduce((acc,task)=>acc+task.duration,0)
+            const totalPay = entryItems.reduce((acc,task)=>acc+task.pay,0)
+            const payPerHour = totalPay/totalTimes || 0
+
+            res.render('dashboard.ejs', {tasks: taskItems, 
+                                         totalTimes: totalTimes, 
+                                         totalPay: totalPay,
+                                         payPerHour: payPerHour, 
+                                         user: req.user
+                                        })
         }catch(err){
             console.log(err)
         }
