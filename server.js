253c744d111e4af53a2f0fe0d0faf8e85281ca68
Austin_Curran <a.curran1192@gmail.com>
2022-10-29 14:15:58 -0600

    deployed to fly.io

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ app.use('/tasks', tasksRoutes)
 app.use('/entries', entriesRoutes)
 
 //Server running  
-app.listen(process.env.PORT, ()=>{
+app.listen(process.env.PORT,"0.0.0.0", ()=>{
     console.log(`Server is running on port ${process.env.PORT}.`)
 })    
