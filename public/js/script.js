b3fa225e41fa59913790998baba8a78aa56b07b3
Austin_Curran <a.curran1192@gmail.com>
2023-10-31 16:58:55 -0600

    implimented chart.js on dashboard

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,4 @@
+/// Timer script ///
 let startBtn = document.querySelector("#start");
 let pauseBtn = document.querySelector("#pause");
 let stopBtn = document.querySelector("#reset");
@@ -11,37 +12,37 @@ let secTenth = document.querySelector("#secTenth");
 let timer = new easytimer.Timer();
 
 timer.addEventListener("secondTenthsUpdated", () => {
-  const obj = timer.getTimeValues();
+	const obj = timer.getTimeValues();
 
-  hours.innerText = obj.hours.toString().padStart(2, "0");
-  minutes.innerText = obj.minutes.toString().padStart(2, "0");
-  seconds.innerText = obj.seconds.toString().padStart(2, "0");
-  secTenth.innerText = obj.secondTenths.toString().padStart(2, "0");
+	hours.innerText = obj.hours.toString().padStart(2, "0");
+	minutes.innerText = obj.minutes.toString().padStart(2, "0");
+	seconds.innerText = obj.seconds.toString().padStart(2, "0");
+	secTenth.innerText = obj.secondTenths.toString().padStart(2, "0");
 });
 
 function recordTime() {
-  let time = timer.getTimeValues(hours, minutes);
-  return time;
+	let time = timer.getTimeValues(hours, minutes);
+	return time;
 }
 
 function insertTime() {
-  let duration = document.getElementById("duration");
-  let time = recordTime();
+	let duration = document.getElementById("duration");
+	let time = recordTime();
 
-  duration.value = time;
+	duration.value = time;
 }
 
 startBtn.addEventListener("click", () => {
-  timer.start({
-    precision: "secondTenths",
-  });
+	timer.start({
+		precision: "secondTenths",
+	});
 });
 pauseBtn.addEventListener("click", () => {
-  timer.pause();
+	timer.pause();
 });
 stopBtn.addEventListener("click", () => {
-  timer.stop();
+	timer.stop();
 });
 recordBtn.addEventListener("click", () => {
-  insertTime();
+	insertTime();
 });
