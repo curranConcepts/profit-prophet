23da48241c2751d5680f50375000f3a08012ac9b
Austin_Curran <a.curran1192@gmail.com>
2023-11-03 15:34:54 -0600

    implimented db info for chart

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -50,11 +50,80 @@ module.exports = {
             const totalTime = entryItems.reduce((acc,task)=>addTimes(acc,task.duration),0) || '00:00:00' //getting error when 0 entries
             const totalHours = totalTime.split(':')[0] || 0
             const PerHour = totalPay / totalHours || 0
-            const payPerHour = Math.round(PerHour * 100) / 100 || 0
+            const payPerHour = (Math.round(PerHour * 100) / 100).toFixed(2) || 0
 
-            // Chart.js
-            //const chartLabels =
-            //const chartData =
+            // Chart.js Queries
+            const previousWeek = await Entries.find({"date":{$gte: new Date((new Date().getTime() - (7 * 24 * 60 * 60 * 1000)))}}).sort({ "date": 1 })
+            const previousMonth = await Entries.find({"date":{$gte: new Date((new Date().getTime() - (30 * 24 * 60 * 60 * 1000)))}}).sort({ "date": 1 })
+            const previousYear = await Entries.find({"date":{$gte: new Date((new Date().getTime() - (365 * 24 * 60 * 60 * 1000)))}}).sort({ "date": 1 })
+
+            // Days Chart
+            const dayData = previousWeek.map(entry => ({
+                x: entry.date.toLocaleDateString(),
+                y: parseInt(entry.duration.split(':')[0])
+            }));
+
+            const reducedDay = Object.values(dayData.reduce((c, { x, y }) => {
+                c[x] = c[x] || {x,y: 0};
+                c[x].y += y;
+                return c;
+            }, {}));
+
+            const dayResult = reducedDay.map(entry => ({
+                x: Date.parse(entry.x),
+                y: entry.y
+            }));
+
+            // Monthly Chart with Weekly Aggregation and 'MMM D' format
+            const monthData = previousMonth.map((entry) => {
+                const date = new Date(entry.date);
+                const weekStart = getWeekStartDate(date);
+                return {
+                    x: weekStart.toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' }),
+                    y: parseInt(entry.duration.split(':')[0]),
+                };
+            });
+
+            const reducedMonth = Object.values(
+                monthData.reduce((c, { x, y }) => {
+                    c[x] = c[x] || { x, y: 0 };
+                    c[x].y += y;
+                    return c;
+                }, {})
+            );
+
+            const weekResult = reducedMonth.map((entry) => ({
+                x: Date.parse(entry.x),
+                y: entry.y,
+            }));
+
+            // Function to get the first day of the week for a date
+            function getWeekStartDate(date) {
+                const d = new Date(date);
+                const dayOfWeek = d.getDay();
+                const diff = d.getDate() - dayOfWeek + (dayOfWeek == 0 ? -6 : 1); // Adjust for Sunday
+                d.setDate(diff);
+                return d;
+            }
+
+            // Year Chart with Monthly Aggregation
+            const yearData = previousYear.map((entry) => ({
+                month: entry.date.toLocaleDateString('en-US', { year: 'numeric', month: 'long' }),
+                y: parseInt(entry.duration.split(':')[0]),
+            }));
+
+            const reducedYear = Object.values(
+                yearData.reduce((c, { month, y }) => {
+                    c[month] = c[month] || { month, y: 0 };
+                    c[month].y += y;
+                    return c;
+                }, {})
+            );
+
+            const monthResult = reducedYear.map((entry) => ({
+                x: Date.parse(entry.month),
+                y: entry.y,
+            }));
 
             res.render('dashboard.ejs', {
                                          entries: entryItems,
@@ -62,6 +131,9 @@ module.exports = {
                                          totalTime: totalTime,
                                          totalPay: totalPay,
                                          payPerHour: payPerHour,
+                                         dayResult: dayResult,
+                                         weekResult: weekResult,
+                                         monthResult: monthResult,
                                          user: req.user
                                         })
         }catch(err){
